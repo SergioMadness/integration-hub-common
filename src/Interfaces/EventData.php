b1812ca136f6d5faa030b5850a51bbaef50db5ed
SergioMadness <madness.sergio@gmail.com>
2018-12-12 22:31:02 +0300

    Move interfaces from DB

--- src/Interfaces/EventData.php
+++ src/Interfaces/EventData.php
@@ -1,11 +1,23 @@
 <?php namespace professionalweb\IntegrationHub\IntegrationHubCommon\Interfaces;
 
+use professionalweb\IntegrationHub\IntegrationHubCommon\Interfaces\Models\Model;
+
 /**
  * Interface for object has data
  * @package professionalweb\IntegrationHub\IntegrationHubCommon\Interfaces
  */
-interface EventData
+interface EventData extends Model
 {
+    public const STATUS_NEW = 'new';
+
+    public const STATUS_QUEUE = 'queue';
+
+    public const STATUS_SUCCESS = 'success';
+
+    public const STATUS_FAILED = 'failed';
+
+    public const STATUS_RETRY = 'need_another_attempt';
+
     /**
      * Get event id
      *
@@ -33,9 +45,53 @@ interface EventData
      * Get value by key
      *
      * @param string $key
-     * @param mixed   $default
+     * @param mixed  $default
      *
      * @return mixed
      */
     public function get(string $key, $default = null);
+
+    /**
+     * Get current flow id
+     *
+     * @return mixed
+     */
+    public function getCurrentFlow();
+
+    /**
+     * Get current step id
+     *
+     * @return mixed
+     */
+    public function getCurrentStep();
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     *
+     * @return EventData
+     */
+    public function setStatus(string $status): self;
+
+    /**
+     * Set current step
+     *
+     * @param $flowId
+     * @param $stepId
+     *
+     * @return EventData
+     */
+    public function setCurrentStep($flowId, $stepId): self;
+
+    /**
+     * Set process response
+     *
+     * @param      $processId
+     * @param      $response
+     * @param bool $succeed
+     *
+     * @return EventData
+     */
+    public function setProcessResponse($processId, $response, $succeed = true): self;
 }
