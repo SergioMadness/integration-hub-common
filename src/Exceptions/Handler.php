75ac5327b42c178523974b5069c12f00266b2d3b
SergioMadness <madness.sergio@gmail.com>
2020-02-05 19:37:46 +0300

    Add report method to handler

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -23,6 +23,21 @@ class Handler extends ExceptionHandler
     ];
 
     /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception $e
+     *
+     * @return void
+     * @throws Exception
+     */
+    public function report(\Exception $e)
+    {
+        parent::report($e);
+    }
+
+    /**
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request $request
