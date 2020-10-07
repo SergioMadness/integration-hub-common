a90be659e98e2dfaf725607b3ff52652cd5fd082
Sergey Zinchenko <s.zinchenko@e-mba.ru>
2020-10-07 18:13:02 +0300

    Add trait for subsystems

---
+++ src/Traits/HasProcessOptions.php
@@ -0,0 +1,39 @@
+<?php namespace professionalweb\IntegrationHub\IntegrationHubCommon\Traits;
+
+use professionalweb\IntegrationHub\IntegrationHubCommon\Interfaces\Services\Subsystem;
+use professionalweb\IntegrationHub\IntegrationHubCommon\Interfaces\Models\ProcessOptions;
+
+/**
+ * Trait for subsystems
+ * @package professionalweb\IntegrationHub\IntegrationHubCommon\Traits
+ * @mixin Subsystem
+ */
+trait HasProcessOptions
+{
+    /**
+     * @var ProcessOptions
+     */
+    private $processOptions;
+
+    /**
+     * Set options with values
+     *
+     * @param ProcessOptions $options
+     *
+     * @return Subsystem
+     */
+    public function setProcessOptions(ProcessOptions $options): Subsystem
+    {
+        $this->processOptions = $options;
+
+        return $this;
+    }
+
+    /**
+     * @return ProcessOptions
+     */
+    public function getProcessOptions(): ProcessOptions
+    {
+        return $this->processOptions;
+    }
+}
